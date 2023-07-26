ef0c60534897b9d13c1c1ed0b4d359becd7b19b9
Buckminsterfullereno <ccesarpadilla@gmail.com>
2023-07-26 14:45:55 -0500

    fixed bugs. This iteration is the first working version.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const container = document.querySelector(" .container");
-const search = document.querySelector(" .search-box button");
-const weatherBox = document.querySelector(" .weather-box");
-const weatherDetails = document.querySelector(" .weather-details");
-const error404 = document.querySelector(" .not-found");
+const container = document.querySelector(".container");
+const search = document.querySelector(".search-box button");
+const weatherBox = document.querySelector(".weather-box");
+const weatherDetails = document.querySelector(".weather-details");
+const error404 = document.querySelector(".not-found");
 
 search.addEventListener("click", () => {
 	const APIKey = "5ee52e24f59d3e4cd13d823463b1d752";
 	const city = document.querySelector(".search-box input").value;
 
-	if (city === " ") return;
+	if (city === "") return;
 
 	fetch(
-		`api.openweathermap.org/data/3.0/weather?q=${city}&units=imperial&appid=${APIKey}`
+		`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`
 	)
 		.then((response) => response.json())
 		.then((json) => {
@@ -25,10 +25,10 @@ search.addEventListener("click", () => {
 			}
 
 			error404.style.display = "none";
-			error404.classList.add("fadeIn");
+			error404.classList.remove("fadeIn");
 
 			const image = document.querySelector(".weather-box img");
-			const temperature = document.querySelector(".weather-box . temperature");
+			const temperature = document.querySelector(".weather-box .temperature");
 			const description = document.querySelector(".weather-box .description");
 			const humidity = document.querySelector(
 				".weather-details .humidity span"
@@ -43,7 +43,7 @@ search.addEventListener("click", () => {
 				case "Clouds":
 					image.src = "Images/cloud.png";
 					break;
-				case "Mist":
+				case "Haze":
 					image.src = "Images/mist.png";
 					break;
 				case "Rain":
@@ -57,7 +57,7 @@ search.addEventListener("click", () => {
 					image.src = "";
 			}
 
-			temperature.innerHTML = `${parseInt(json.main.temp)}<span>F</span>`;
+			temperature.innerHTML = `${parseInt(json.main.temp)}<span>°F</span>`;
 			description.innerHTML = `${json.weather[0].description}`;
 			humidity.innerHTML = `${json.main.humidity}%`;
 			wind.innerHTML = `${parseInt(json.wind.speed)} km/hr`;
@@ -65,7 +65,7 @@ search.addEventListener("click", () => {
 			weatherBox.style.display = "";
 			weatherDetails.style.display = "";
 			weatherBox.classList.add("fadeIn");
-			weatherDetails.classList.add("FadeIn");
+			weatherDetails.classList.add("fadeIn");
 			container.style.height = "590px";
 		});
 });
